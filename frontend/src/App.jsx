2119bd5eb2f1fc89c9446d98694af1a974cf2198
yopaz-khainh <nguyenhuukhai1303@gmail.com>
2025-08-27 15:42:16 +0700

    refactor: rearrange component structure in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,20 +8,20 @@ const { BASE_URL } = import.meta.env;
 
 export function App() {
   return (
-    <BrowserRouter basename={BASE_URL}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="light"
-        storageKey="vite-theme"
-        enableSystem
-        disableTransitionOnChange
-        enableColorScheme
-      >
-        <LoadingBarContainer>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      storageKey="vite-theme"
+      enableSystem
+      disableTransitionOnChange
+      enableColorScheme
+    >
+      <LoadingBarContainer>
+        <BrowserRouter basename={BASE_URL}>
           <Toaster />
           <AppRouting />
-        </LoadingBarContainer>
-      </ThemeProvider>
-    </BrowserRouter>
+        </BrowserRouter>
+      </LoadingBarContainer>
+    </ThemeProvider>
   );
 }
