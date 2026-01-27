5b8e5e4b5f24f2c1fddc177b8d312667f23ff389
yopaz-quanlv <quanlv@yopaz.vn>
2026-01-27 10:40:01 +0000

    fix build

--- backend/internal/database/seed.go
+++ backend/internal/database/seed.go
@@ -1,16 +1,6 @@
 package database
 
-import (
-    "fmt"
-
-    // "gorm.io/gorm"
-
-    // "metronic/internal/model"
-    // "metronic/internal/security"
-)
-
 // Seed creates default roles and demo users for quick login
 func Seed() error {
    return nil
 }
-
