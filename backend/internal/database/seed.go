d3c48db70ef8dba5a00cdd8de3690e9fcfde1257
yopaz-quanlv <quanlv@yopaz.vn>
2026-04-02 03:56:00 +0000

    ok

--- backend/internal/database/seed.go
+++ backend/internal/database/seed.go
@@ -1,6 +1,9 @@
 package database
 
+import "gorm.io/gorm"
+
 // Seed creates default roles and demo users for quick login
-func Seed() error {
-   return nil
+func Seed(db *gorm.DB) error {
+	_ = db
+	return nil
 }
